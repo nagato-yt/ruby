85cd279b1480fb797cd9582eba58faa9ee1bcb90
nagato-yt <119833402+nagato-yt@users.noreply.github.com>
2022-12-23 13:22:47 +0000

    update

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,6 +1,17 @@
-def fizz_buzz(number)
+def fizz_buzz(number) 
+    if number%15==0
+        puts "FizzBuzz"
     
-end
+    elsif number%5==0
+        puts "Buzz"
+        
+    elsif number%3==0
+    puts "Fizz"
+ 
+    else
+        puts number
+    end 
+end 
 puts "数字を入力してください"
 
 input =gets.to_i
